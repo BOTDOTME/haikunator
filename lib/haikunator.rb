58cc350a4ad54ab1d2e0b8c0ac636761d27a9e58
Zolon Wilkins <zolonsk8@gmail.com>
2017-07-12 17:14:08 -0500

    first round of words

--- lib/haikunator.rb
+++ lib/haikunator.rb
@@ -29,27 +29,17 @@ module Haikunator
 
     def adjectives
       %w(
-        autumn hidden bitter misty silent empty dry dark summer
-        icy delicate quiet white cool spring winter patient
-        twilight dawn crimson wispy weathered blue billowing
-        broken cold damp falling frosty green long late lingering
-        bold little morning muddy old red rough still small
-        sparkling throbbing shy wandering withered wild black
-        young holy solitary fragrant aged snowy proud floral
-        restless divine polished ancient purple lively nameless
+        pool car can mtn spkr fan dry cord wire sun desk
+        head ice wtr trip flip surf sand sea tv palm fire
+        wind fart toot butt
       )
     end
 
     def nouns
       %w(
-        waterfall river breeze moon rain wind sea morning
-        snow lake sunset pine shadow leaf dawn glitter forest
-        hill cloud meadow sun glade bird brook butterfly
-        bush dew dust field fire flower firefly feather grass
-        haze mountain night pond darkness snowflake silence
-        sound sky shape surf thunder violet water wildflower
-        wave water resonance sun wood dream cherry tree fog
-        frost voice paper frog smoke star
+        cool lit poor sad fun sick meh ok dope chill lit
+        good nice neat fun gnar eww boom fast fizz fun glee
+        kind mild pro pure top
       )
     end
   end
